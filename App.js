df1a5988a3578af437d2c86df883967d366425cb
Tunahan Çeler <tunaceler@gmail.com>
2024-07-28 17:24:15 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>İngilizce Öğrenmeye Hazır Mısınız</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:"blue"
+  },
+  text:{
+    color:"white",
+    fontSize:24,
+    fontWeight:"bold",
+  }
+});
